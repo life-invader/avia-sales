c3e8f06d32ba99e9cbd2efd8c25b1cdc0361b0c7
Alexander <orientstar96@gmail.com>
2022-07-23 02:28:34 +0500

    3.9 Обновляет компоненты 'Лоадер' и 'Ошибка'

--- src/components/loader/loader.tsx
+++ src/components/loader/loader.tsx
@@ -4,18 +4,13 @@ import './loader.scss';
 function Loader() {
   return (
     <ContentLoader
-      className="loader ticket"
-      speed={1}
-      width={502}
-      height={135}
+      className="loader"
+      speed={1.5}
       viewBox="0 0 502 135"
       backgroundColor="#ffffff"
-      foregroundColor="#2e2828"
+      foregroundColor="#2196f3"
     >
-      <rect x="0" y="80" rx="3" ry="3" width="462" height="18" />
-      <rect x="0" y="110" rx="3" ry="3" width="462" height="21" />
-      <rect x="362" y="20" rx="3" ry="3" width="110" height="36" />
-      <rect x="0" y="20" rx="3" ry="3" width="82" height="21" />
+      <rect x="0" y="0" rx="0" ry="0" width="502" height="135" />
     </ContentLoader>
   );
 }
